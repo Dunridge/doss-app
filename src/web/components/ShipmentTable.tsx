1f6b1ceda97cdb8e85da3140cb5e24a0ab1611eb
Dunridge <Polinka7max@gmail.com>
2024-05-23 08:55:24 -0700

    view-tables. add table styles

--- src/web/components/ShipmentTable.tsx
+++ src/web/components/ShipmentTable.tsx
@@ -1,5 +1,6 @@
 import { IShipmentTableProps } from "../utils/interfaces/IShipmentTableProps";
 import Shipment from "./Shipment";
+import "../style/ShipmentTable.css";
 
 export default function ShipmentTable({ buildNumber, id, shipments }: IShipmentTableProps) {
 
@@ -7,13 +8,23 @@ export default function ShipmentTable({ buildNumber, id, shipments }: IShipmentT
         <div className="ShipmentTable">
             <div className="ShipmentTable__headerRow">
                 <div>Build number: {buildNumber}</div>
-                <span>---</span>
-                <div>id: {id}</div>
+                <div>ID: {id}</div>
             </div>
 
             <div className="ShipmentTable__shipments">
                 <div className="ShipmentTable__shipmentsHeader">Shipments</div>
-                {shipments.map((shipment) => <Shipment key={shipment.id} {...shipment} />)}
+                <table className="ShipmentTable__shipmentsTable">
+                    <thead>
+                        <tr>
+                            <th>Order #</th>
+                            <th>Description</th>
+                            <th>Cost</th>
+                        </tr>
+                    </thead>
+                    <tbody> 
+                        {shipments.map((shipment) => <Shipment key={shipment.id} {...shipment} />)}
+                    </tbody>
+                </table>
             </div>
         </div>
     )
